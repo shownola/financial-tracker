391252547359f007c27c590ba0d8d7bfd7f23d77
shownola <swasieko@gmail.com>
2020-03-08 11:47:12 -0700

    Add devise and create users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # config.action_mailer.default_url_options = { host: 'production url', port: 3000 }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
