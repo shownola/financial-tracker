c4333396ec5f3f2d1f5ad6135508559568eaf872
shownola <swasieko@gmail.com>
2020-03-11 16:00:20 -0700

    Implemented ability to add first and last name to devise users by configuring permitted parameters in the application controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_194019) do
+ActiveRecord::Schema.define(version: 2020_03_11_194045) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_03_10_194019) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
