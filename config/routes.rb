0494bea50f9be749c16950376439c42d871fd53d
shownola <swasieko@gmail.com>
2020-03-18 13:18:58 -0700

    Implemented stock updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_portfolio_update', to: 'users#my_portfolio_update'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_233503) do
+ActiveRecord::Schema.define(version: 2020_03_18_183218) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_233503) do
     t.decimal "last_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "before_price"
   end
 
   create_table "user_stocks", force: :cascade do |t|
