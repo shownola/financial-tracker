820378c79fd45763d2fde3014f6da37ce2341831
shownola <swasieko@gmail.com>
2020-03-18 10:53:31 -0700

    Implemented helper methods for footer and copyright and updated styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_copyright
+
   protected
 
   def configure_permitted_parameters
@@ -9,6 +11,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
-  
+  def set_copyright
+    @copyright = ShownolaViewTool::Renderer.copyright 'Shownola | Sherry Wasieko', 'All rights reserved'
+  end
+
+  module ShownolaViewTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b> #{name}</b> #{msg}" .html_safe
+      end
+    end
+  end
+
+
 
 end
